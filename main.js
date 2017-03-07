767cea8728ec7ada12b2a246acd7181a59a67adf
jmarcusgill <jmarcusgill@gmail.com>
2017-03-06 19:58:44 -0600

    created first xhr request

--- main.js
+++ main.js
@@ -2,3 +2,21 @@ console.log("test poop")
 
 // var x = 5;
 // div.innerHTML = x; (synchronous)
+
+function executeThisCodeAfterFileLoaded() {
+  console.log("IT WORKED!!")
+}
+
+function executeThisCodeAfterFileFails() {
+  console.log("booooooooo")
+}
+
+
+
+var myRequest = new XMLHttpRequest(); //initiates new request
+myRequest.addEventListener("load", executeThisCodeAfterFileLoaded)
+myRequest.addEventListener("error", executeThisCodeAfterFileFails)
+myRequest.open("GET", "dinosaurs.json") //1st: what action you want to do, 2nd: where are you getting it from. linking json to js
+myRequest.send(); //makes the request HAPPEN
+
+console.log("myRequest", myRequest)
